c1955c1e834346a5ab628f41888772afe376415d
hatemboudabra <132352084+hatemboudabra@users.noreply.github.com>
2024-02-15 23:01:50 +0100

    commit for filter

--- Social_Network/db/db.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Social_Network').then(
-    ()=>{
-        console.log('connected!!!');
-    }
-
-);
-module.exports=mongoose
--- Social_Network/server.js
+++ Social_Network/server.js
@@ -1,9 +1,28 @@
-const express = require("express");
-const app = express();
-const mongoose = require('./db/db');
+const express = require ('express')
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-app.use(express.json());
+//app.use( cors())
+dotenv.config()
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000
+const app = express()
+app.use(express.json())
 
+mongoose.connect(MONGODB_URI).then(()=>{
+    console.log('connected to MongoDb');
+    app.listen(PORT,()=>{
+        console.log(`server listening on ${PORT}`)
+    })
+}).catch((err) =>{
+    console.error('Error connecting to mongodb:',err.message)
+})
+app.get('/',(req,res)=>{
+    return res.status(200).send('hello chabeb !')
+})
+    /*
 app.listen(4000,()=>{
     console.log(`server listening on 4000`)
 })
+*/
